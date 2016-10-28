96f425a2bba8807e5d2bc0f79349d94799e97495
eonat <ert.onat@gmail.com>
2016-10-28 14:00:09 +0300

    Test added

--- Assets/Scripts/Tests/Editor/TestIsFinished.cs
+++ Assets/Scripts/Tests/Editor/TestIsFinished.cs
@@ -15,12 +15,88 @@ public class TestIsFinished {
         List<Stone> sl = new List<Stone>();   //Making an arbitrary player hand
         sl.Add(new Stone(1, Stone.StoneColor.Black, false));
         sl.Add(new Stone(2, Stone.StoneColor.Black, false));
+		sl.Add(new Stone(3, Stone.StoneColor.Black, false));
+		sl.Add(new Stone(4, Stone.StoneColor.Black, false));
+		sl.Add(new Stone(4, Stone.StoneColor.Blue, false));
+		sl.Add(new Stone(4, Stone.StoneColor.Green, false));
+		sl.Add(new Stone(4, Stone.StoneColor.Red, false));
+		sl.Add(new Stone(7, Stone.StoneColor.Red, false));
+		sl.Add(new Stone(8, Stone.StoneColor.Red, false));
+		sl.Add(new Stone(9, Stone.StoneColor.Red, false));
+		sl.Add(new Stone(8, Stone.StoneColor.Green, false));
+		sl.Add(new Stone(9, Stone.StoneColor.Green, false));
+		sl.Add(new Stone(10, Stone.StoneColor.Green, false));
+
         //..
         
         bool isF = LogicApi.IsFinished(sl);   //checking if player is finished the game
         if (isF != false) failCount++;        //it we are not happy with the result count the fails
         //---------------------
         //TODO: yukarıdaki gibi en az 10 tane use case yaz
+
+		//-------------------
+		List<Stone> s2 = new List<Stone>();   //Making an arbitrary player hand
+		s2.Add(new Stone(4, Stone.StoneColor.Blue, false));
+		s2.Add(new Stone(5, Stone.StoneColor.Blue, false));
+		s2.Add(new Stone(6, Stone.StoneColor.Blue, false));
+		s2.Add(new Stone(7, Stone.StoneColor.Blue, false));
+		s2.Add(new Stone(8, Stone.StoneColor.Blue, false));
+		s2.Add(new Stone(5, Stone.StoneColor.Black, false));
+		s2.Add(new Stone(6, Stone.StoneColor.Black, false));
+		s2.Add(new Stone(7, Stone.StoneColor.Black, false));
+		s2.Add(new Stone(8, Stone.StoneColor.Black, false));
+		s2.Add(new Stone(10, Stone.StoneColor.Red, false));
+		s2.Add(new Stone(10, Stone.StoneColor.Green, false));
+		s2.Add(new Stone(10, Stone.StoneColor.Black, false));
+		s2.Add(new Stone(10, Stone.StoneColor.Blue, false));
+		s2.Add(new Stone(13, Stone.StoneColor.Blue, false));
+		//..
+
+		isF = LogicApi.IsFinished(s2);   //checking if player is finished the game
+		if (isF != false) failCount++;        //it we are not happy with the result count the fails
+
+
+		//-------------------
+		List<Stone> s3 = new List<Stone>();   //Making an arbitrary player hand
+		s3.Add(new Stone(11, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(12, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(13, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(1, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(3, Stone.StoneColor.Black, false));
+		s3.Add(new Stone(3, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(3, Stone.StoneColor.Red, false));
+		s3.Add(new Stone(3, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(2, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(3, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(4, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(5, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(6, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(7, Stone.StoneColor.Blue, false));
+		//..
+
+		isF = LogicApi.IsFinished(s3);   //checking if player is finished the game
+		if (isF != false) failCount++;        //it we are not happy with the result count the fails
+
+		//-------------------
+		List<Stone> s4 = new List<Stone>();   //Making an arbitrary player hand
+		s3.Add(new Stone(5, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(5, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(5, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(5, Stone.StoneColor.Green, false));
+		s3.Add(new Stone(13, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(1, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(2, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(3, Stone.StoneColor.Blue, false));
+		s3.Add(new Stone(4, Stone.StoneColor.Red, false));
+		s3.Add(new Stone(5, Stone.StoneColor.Red, false));
+		s3.Add(new Stone(6, Stone.StoneColor.Red, false));
+		s3.Add(new Stone(7, Stone.StoneColor.Red, false));
+		s3.Add(new Stone(8, Stone.StoneColor.Red, false));
+		s3.Add(new Stone(9, Stone.StoneColor.Red, false));
+		//..
+
+		isF = LogicApi.IsFinished(s3);   //checking if player is finished the game
+		if (isF != false) failCount++;        //it we are not happy with the result count the fails
 
 
         //TODO: delete this
