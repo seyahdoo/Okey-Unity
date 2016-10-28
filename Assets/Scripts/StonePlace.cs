bf21fea1a6a3a55759c35ca3eb0061f71062699c
Gökhan Çevik <resency@hotmail.com>
2016-10-28 13:53:14 +0300

    Tas sürükleme islemleri

--- Assets/Scripts/StonePlace.cs
+++ Assets/Scripts/StonePlace.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.EventSystems;
 
-public class StonePlace : MonoBehaviour ,IStoneDroppable {
-    
+public class StonePlace : MonoBehaviour ,IStoneDroppable, IDropHandler , IPointerEnterHandler, IPointerExitHandler
+{
+
     public void DropStone(Stone stone)
     {
-        throw new NotImplementedException();
+       
     }
-    
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        StoneRenderer stoneRenderer = eventData.pointerDrag.GetComponent<StoneRenderer>();
+        if (stoneRenderer != null)
+        {
+            stoneRenderer.parentToReturnTo = this.transform;
+            DropStone(stoneRenderer.stone);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+     
+    }
 }
--- Assets/Scripts/StoneRenderer.cs
+++ Assets/Scripts/StoneRenderer.cs
@@ -1,14 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+using System;
 
-public class StoneRenderer : MonoBehaviour {
+public class StoneRenderer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler  {
     
     public Stone stone;
     public bool IsMovable;
     public bool IsHidden;
 
+    public Transform parentToReturnTo = null;
 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent,false);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
 
-    
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(parentToReturnTo, false);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
 }
