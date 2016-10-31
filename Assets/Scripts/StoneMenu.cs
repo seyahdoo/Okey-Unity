fa7172bf65bfd15c75fece196b96cf6bfafd485c
Gökhan Çevik <resency@hotmail.com>
2016-10-31 14:49:58 +0300

    panel eklendi

---
+++ Assets/Scripts/StoneMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StoneMenu : MonoBehaviour
+{
+    public Transform stoneMenuPanel;
+
+    public void StoneMenuCheck()
+    {
+        if (stoneMenuPanel.gameObject.activeSelf)
+            stoneMenuPanel.gameObject.SetActive(false);
+        else if (!stoneMenuPanel.gameObject.activeSelf)
+            stoneMenuPanel.gameObject.SetActive(true);
+    }
+    public void SortStoneNumber()
+    {
+
+    }
+    public void SortStoneColor()
+    {
+      
+    }
+}
