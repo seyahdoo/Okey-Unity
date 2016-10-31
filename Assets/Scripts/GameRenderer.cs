763d22bb6fc5089406f9d8013bc279f8532c5b8b
eonat <ert.onat@gmail.com>
2016-10-31 13:38:53 +0300

    changes

--- Assets/Scripts/GameRenderer.cs
+++ Assets/Scripts/GameRenderer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GameRenderer : MonoBehaviour {
     
-
+	float lerpingtime;
 
     //TODO: Design how to render the game???
     
@@ -19,10 +19,23 @@ public class GameRenderer : MonoBehaviour {
         //TODO: render a stone that is not draggable or changable who will stay on "to"
         //      pulled from pool, spawned at from and will be going to "to" with a speed
 
-
-
+		GameObject go = Pool.Get("Stone");
+		if (!isHidden) {
+			go.GetComponent<StoneRenderer> ().Render ();
+		}
+		StartCoroutine(Move(go));
     }
 
+	IEnumerator Move(GameObject go)
+
+	{	
+		while(lerpingtime < 3)
+		{	
+			go.transform.position = Vector3.Lerp(from, to, lerpingtime/3);
+			lerpingtime += Time.deltaTime;
+			yield return new WaitForEndOfFrame ();
+		}
+	}
 
 
 
